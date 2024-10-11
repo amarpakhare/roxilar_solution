bdf2fbe743e11d7378e132a1203d86c7bec09977
amarpakhare <amarpakhare30@gmail.com>
2024-10-11 12:52:53 +0530

    added margin to statistics component

--- frontend/src/components/StatisticsTable.jsx
+++ frontend/src/components/StatisticsTable.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const StatisticsTable = ({statistics}) => {
 	return (
-		<div className="bg-white shadow-lg rounded-lg p-6 md:p-8 lg:p-10 mb-6">
+		<div className="bg-white shadow-lg rounded-lg p-6 md:p-8 lg:p-10 mb-6 mt-6">
 			<h2 className="text-2xl font-bold text-gray-800 mb-4">Statistics</h2>
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 				<div className="bg-blue-100 p-4 rounded-lg">
